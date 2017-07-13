914f83453fbbaa190889adcccebb49ab38d0f1bb
Mohan Singh <singhmohancs@gmaIl.com>
2017-07-13 14:01:54 +0530

    @backand/angular2-sdk Integration

--- src/+app/app.component.ts
+++ src/+app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, Directive, ElementRef, Renderer, ChangeDetectionStrategy, ViewEncapsulation } from '@angular/core';
-
+import { BackandService } from '@backand/angular2-sdk'
 //
 /////////////////////////
 // ** Example Directive
@@ -63,4 +63,21 @@ export class XLargeDirective {
 })
 export class AppComponent {
   title = 'ftw';
+  res: string;
+  constructor(private backand: BackandService) {}
+  ngOnInit() {
+     this.backand.init({
+      appName: 'funhub',
+      anonymousToken: 'f10673bb-d12a-4245-8eca-312add606059',
+    });
+    this.getList();
+  }
+
+   getList(): void {
+    this.res = 'fetching objects...';
+    this.backand.object.getList('users').then((res: any) => {
+      this.res = `${res.data.length} objects fetched`;
+      console.log(res);
+    })
+  }
 }
